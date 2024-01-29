4476f0586357ecf0c9a9a4e26c1a3f8c53923710
zangzoo <20211397@sungshin.ac.kr>
2024-01-30 00:19:41 +0900

    4장 구현) 실전문제들

--- 4-3 Implementation.py
+++ 4-3 Implementation.py
@@ -1,1 +1,20 @@
 # 왕실의 나이트 문제
+
+#현재 나이트의 위치 입력받기
+input_data=input()
+row = int(input_data[1]) #행은 입력값의 인덱스[1]
+column = int(ord(input_data[0])-int(ord('a'))) +1 #ord: 문자열을 숫자로 바꿔줌! a를 빼고 1을 더해서 a부터 1이 될 수 있도록!
+
+#나이트가 이동할 수 있는 방향 정의
+steps = [(2,-1),(2,1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
+
+#위의 8가지 방향에 대해 각 위치로 이동이 가능한지 확인
+result=0
+for step in steps:
+    #이동하고자 하는 위치 확인
+    next_row=row+step[0]
+    next_col=column+step[1]
+    #해당 위치로 이동이 가능하면 result 1 증가
+    if next_row>=1 and next_row<=8 and next_col>=1 and next_col<=8:
+        result+=1
+print(result)
